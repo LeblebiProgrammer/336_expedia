771c5ea838199fd660a0300ab67e18b9da0731fc
yuhonjacklin <53705194+yuhonjacklin@users.noreply.github.com>
2019-08-07 11:58:15 -0400

    Updated SQL query

--- src/spack/AdminPages/ReservationByName.java
+++ src/spack/AdminPages/ReservationByName.java
@@ -1,11 +1,20 @@
 package spack.AdminPages;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.ResultSetMetaData;
 
 /**
  * Servlet implementation class ReservationByName
@@ -17,6 +26,15 @@ public class ReservationByName extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
+	private String aic = "";
+	
+	private void setCode(String code) {
+		aic = code;
+	}
+	
+	private String getCode() {
+		return aic;
+	}
      
      protected void initialHtml(java.io.PrintWriter out) {
 	    	String str = "\n" + 
@@ -24,43 +42,78 @@ public class ReservationByName extends HttpServlet {
 	    			"<html>\n" + 
 	    			"<head>\n" + 
 	    			"<meta charset=\"UTF-8\">\n" + 
-	    			"<title>Manage Airlines</title>\n" + 
+	    			"<title>Reservation by name</title>\n" + 
 	    			"</head>\n" + 
 	    			"<body>\n" + 
-	    			"	<form action=\"ManageAirlines\" method=\"post\">\n" + 
-	    	 
-	    	"			<table style=\"height: 51px; width: 100px; float: left;\" border=\"1\">\n" + 
-	    	"				<tbody>\n" + 
-	    	"<tr style=\"height: 27px;\">\n" + 
-	    	"					<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
-	    	"						name=\"click\" type=\"submit\" value=\"BookUser\" /></td>\n" + 
-	    	"				</tr>\n" + 
-	    	"				<tr style=\"height: 27px;\">\n" + 
-	    	"					<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
-	    	"						name=\"click\" type=\"submit\" value=\"ChangeFlight\" /></td>\n" + 
-	    	"				</tr>\n" + 
-	    	"				<tr style=\"height: 27px;\">\n" + 
-	    	"					<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
-	    	"						name=\"click\" type=\"submit\" value=\"ManageFlights\" /></td>\n" + 
-	    	"				</tr>\n" + 
-	    	"				<tr style=\"height: 27px;\">\n" + 
-	    	"					<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
-	    	"						name=\"click\" type=\"submit\" value=\"ManageAirports\" /></td>\n" + 
-	    	"				</tr>\n" + 
-	    	"				<tr style=\"height: 27px;\">\n" + 
-	    	"					<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
-	    	"						name=\"click\" type=\"submit\" value=\"ManageAirlines\" /></td>\n" + 
-	    	"				</tr>\n" + 
-	    	"				<tr style=\"height: 27px;\">\n" + 
-	    	"					<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
-	    	"						name=\"click\" type=\"submit\" value=\"ManageAirplanes\" /></td>\n" + 
-	    	"				</tr>\n" + 
-	    	"				<tr style=\"height: 27px;\">\n" + 
-	    	"					<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
-	    	"						name=\"click\" type=\"submit\" value=\"GetWaitlist\" /></td>\n" + 
-	    	"				</tr>"+
-	    	"				</tbody>\n" + 
-	    	"			</table></div>\n" ;
+	    			"	<form action=\"ReservationByName\" method=\"post\">\n" + 
+	    	 "			<table style=\"height: 51px; width: 100px; float: left;\" border=\"1\">\n" + 
+			"				<tbody>\n" + 
+			"					<tr style=\"height: 27px;\">\n" + 
+			"						<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
+			"							name=\"click\" type=\"submit\" value=\"Manage users\" /></td>\n" + 
+			"					</tr>\n" + 
+			"					<tr style=\"height: 27px;\">\n" + 
+			"						<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
+			"							name=\"click\" type=\"submit\" value=\"GetSales\" /></td>\n" + 
+			"					</tr>\n" + 
+			"					<tr style=\"height: 27px;\">\n" + 
+			"						<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
+			"							name=\"click\" type=\"submit\" value=\"GetReservations\" /></td>\n" + 
+			"					</tr>\n" + 
+			"					<tr style=\"height: 27px;\">\n" + 
+			"						<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
+			"							name=\"click\" type=\"submit\" value=\"GetRevenue\" /></td>\n" + 
+			"					</tr>\n" + 
+			"					<tr style=\"height: 27px;\">\n" + 
+			"						<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
+			"							name=\"click\" type=\"submit\" value=\"GetFlightHistory\" /></td>\n" + 
+			"					</tr>\n" + 
+			"					<tr style=\"height: 27px;\">\n" + 
+			"						<td style=\"width: 260px; height: 27px; text-align: center;\"><input\n" + 
+			"							name=\"click\" type=\"submit\" value=\"GetAllFlights\" /></td>\n" + 
+			"					</tr>\n" + 
+			"\n" + 
+			"				</tbody>\n" + 
+			"			</table></div>\n"+
+			
+	"			<table style=\"height: 51px; width: 100px; float: left;\" border=\"1\">\n" + 
+	"				<tbody>\n" + 
+	"					<tr style=\"height: 27px;\">\n" + 
+	"						<td style=\"width: 260px; height: 27px; text-align: center;\">Search for airport</td>\n";
+	
+	    	str += "<td width = \"14%\"><select name = \"Code\" size = \"1\">";
+	    	
+	    	
+	    	
+	    	//departure
+	    	while(airport.next()) {
+	    		//String option= "<option value = \"" + aircraft.getString("TailNumber") + "\"" + aircraft.getString("TailNumber") + "</option>";\
+	    		try {
+	    			if (airport.getString("AirportCode").equals(this.getCode())) {
+						String option = "<option value = \"" + airport.getString("AirportCode") + "\" selected>"
+								+ airport.getString("AirportName") + "</option>\n";
+						str += option;
+					} else {
+	    			String option = "<option value = \"" + airport.getString("AirportCode") + "\">" + airport.getString("AirportName") + "</option>\n";
+	    			str += option;
+					}
+	    		}
+	    		catch(Exception e) {
+	    			System.out.print(e.getMessage());
+	    		}
+	    	}
+	    	str += "</select></td>";
+	
+	
+	
+	
+	str += "						<td>	<input name=\"click\" type=\"Submit\" value=\"Search\" /></td>\n"+
+	"					</tr>\n" + 
+	"\n" + 
+	"				</tbody>\n" + 
+	"			</table></div>\n";
+			
+			;
 	    	
 	    	out.print(str);
 	    }
@@ -140,8 +193,9 @@ public class ReservationByName extends HttpServlet {
 			
 			PreparedStatement stmt;	
 			
-			stmt=con.prepareStatement("select r.Name from Reservation r where r.Name=<INPUT>");  
-			
+			stmt=con.prepareStatement("select ut.FirstName, ut.LastName from UserTable ut where ut.FirstName = ? AND ut.LastName = ?");  
+			stmt.setString(1, this.getCode());
+			stmt.setString(2, getCode());
 			
 			ResultSet rs = stmt.executeQuery();
 			
